db881b59dad14c59cb60c7611a449e2d485afdd3
Angelo Ricardo <angeloengcomp@gmail.com>
2021-05-21 03:16:28 -0300

    att

--- Semana-do-JavaScript/reduce.js
+++ Semana-do-JavaScript/reduce.js
@@ -1,5 +1,31 @@
 const numbers = [1, 2, 3]
 
-const sumNumbers  = numbers.reduce((accumalator, item)=>accumalator+item, 0)
+const sumNumbers = numbers.reduce((accumalator, item) => accumalator + item, 0)
 
 console.log(sumNumbers)
+
+// ------------------------------------
+
+
+
+const phaseScores = [
+    { name: 'Vinicius Costa', score: 337 },
+    { name: 'Roger Melo', score: 43 },
+    { name: 'Alfredo Braga', score: 234 },
+    { name: 'Pedro H. Silva', score: 261 },
+    { name: 'Ana Paula Rocha', score: 491 },
+    { name: 'Vinicius Costa', score: 167 },
+    { name: 'Roger Melo', score: 137 },
+    { name: 'Alfredo Braga', score: 135 },
+    { name: 'Ana Paula Rocha', score: 359 },
+    { name: 'Pedro H. Silva', score: 133 }
+]
+
+const scoreRogermelo = phaseScores.reduce((accumalator, phaseScore) => {
+    if (phaseScore.name === 'Roger Melo') {
+        accumalator += phaseScore.score
+    }
+    return accumalator
+
+}, 0)
+console.log(scoreRogermelo)
