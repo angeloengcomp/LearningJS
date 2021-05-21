b20f7b668c85e9cd158846e92469b23ce54d1cc6
Angelo Ricardo <angeloengcomp@gmail.com>
2021-05-21 00:06:43 -0300

    att

---
+++ Semana-do-JavaScript/map.js
@@ -0,0 +1,19 @@
+const lista = [1, 2, 3]
+
+console.log(lista)
+
+const doubleLista =  lista.map(item =>  item * 2 )
+
+console.log(doubleLista)
+
+
+
+const elements = ['hidrogenio','oxigenio','ferro', 'berilium']
+
+const tamanho = elements.map(
+    itens => 
+    itens.length
+)
+
+console.log(tamanho)
+
