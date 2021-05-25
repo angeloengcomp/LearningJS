c0701940a9d23d76f8393d1b45a9a6f3af4a4838
Angelo Ricardo <angeloengcomp@gmail.com>
2021-05-24 21:24:10 -0300

    Create array.js

---
+++ js-tp-basic-to-advanced/array.js
@@ -0,0 +1,9 @@
+const listaNumeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+console.log(Array.isArray(listaNumeros)) // diz se é um array = true
+
+listaNumeros.length // tamamho do array = 10
+
+console.log(listaNumeros[1])
+
+
