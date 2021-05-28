1258cc2bc7b08834fdcd2788b4ec8c84b4077bb2
Angelo Ricardo <angeloengcomp@gmail.com>
2021-05-28 00:25:19 -0300

    att

--- js-tp-basic-to-advanced/exercicio-final-basico.js
+++ js-tp-basic-to-advanced/exercicio-final-basico.js
@@ -1,2 +1,39 @@
-const nome = 'luiz';
+function meuEscopo(){
+    const form = document.querySelector('form')
+    const resultado = document.querySelector('.resultado')
 
+    const pessoas = []
+
+
+
+    // form.onsubmit = function (evento){
+    //     evento.preventDefault()
+    //     alert('enviado')
+    // }
+    
+    function recebeEventoForm(evento){
+        evento.preventDefault()  //previne que o formulario reset a pagina
+
+        const nome = form.querySelector('.nome')
+        const sobrenome = form.querySelector('.sobrenome')
+        const peso = form.querySelector('.peso')
+        const altura = form.querySelector('.altura')
+
+        pessoas.push({
+            nome:nome.value,
+            sobrenome:sobrenome.value,
+            peso:peso.value,
+            altura:altura.value
+        })
+
+        console.log(pessoas)
+
+        resultado.innerHTML += `<p>nome: ${nome.value}/ sobrenome: ${sobrenome.value}/ peso: ${peso.value}/ altura: ${altura.value}</p>`
+
+    }
+
+    form.addEventListener('submit',recebeEventoForm)//executa algo qnd acontece determinado evento
+    
+}
+
+meuEscopo()
