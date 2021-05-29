c721091d2be0f8de996c2ff91aa3791bdac91a0b
Angelo Ricardo <angeloengcomp@gmail.com>
2021-05-29 01:19:46 -0300

    operadores

---
+++ js-tp-basic-to-advanced/1-operadores-comparacao.js
@@ -0,0 +1,11 @@
+// operadores de comparacao
+// > maior que 
+// >= maior ou igual
+// <= menor ou igual
+// < menor que
+// == igualdade
+// === igualdade restrita (valor e tipo)
+// != diferente
+// !== diferente restrito (valor e tipo)
+
+console.log(9 >= 3)//true
---
+++ js-tp-basic-to-advanced/2-operadores-logicos.js
@@ -0,0 +1,12 @@
+// operadorres logicos
+// && AND (E)
+// || OR  (OU)
+// ! NOT (NÃO)
+
+console.log(10 < 20 && 6 != 2)//true
+
+console.log(10 > 1 || 5 == 2)//true
+
+console.log(!false)//true
+console.log(!true)//false
+console.log(!(2==2))//false
