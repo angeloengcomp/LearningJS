2bb29f32baca1efacdd4b76d9e3c165a37ff54c6
Angelo Ricardo <50966170+angeloengcomp@users.noreply.github.com>
2021-06-08 13:29:34 -0300

    cronometro v1.0

--- js-tp-basic-to-advanced/Fundamentos/20_cronometro.js
+++ js-tp-basic-to-advanced/Fundamentos/20_cronometro.js
@@ -1,26 +1,67 @@
+function criaHoraDosSegundos(segundos) {
+    const data = new Date(segundos * 1000);
+
+    return data.toLocaleTimeString('pt-BR',
+        {
+            hour12: false,
+            timeZone: 'UTC'
+        }
+    )
+}
+
 const relogio = document.querySelector('.relogio');
 const iniciar = document.querySelector('.iniciar');
 const pausar = document.querySelector('.pausar');
 const zerar = document.querySelector('.zerar');
+let segundos = 0;
+let timer;
+
+function iniciaRelogio() {
+    timer = setInterval(function () {
+        segundos++;
+        relogio.innerHTML = criaHoraDosSegundos(segundos);
+    }, 1000);
+}
+
+// iniciar.addEventListener('click', function (event) {
+//     relogio.classList.remove('pausado');
+//     clearInterval(timer);
+//     iniciaRelogio();
+// });
+
+// pausar.addEventListener('click', function (event) {
+//     relogio.classList.add('pausado')
+//     clearInterval(timer);
+// });
+
+// zerar.addEventListener('click', function (event) {
+//     clearInterval(timer);
+//     relogio.innerHTML = '00:00:00';
+//     segundos = 0;
+// });
 
 
-let data = new Date();
-let time = data.
 
 
 
+// Codigo simplificado
+document.addEventListener('click', function (evento) {
+    const elemento = evento.target;
 
-iniciar.addEventListener('click', function () {
-    relogio.innerHTML = 'cliquei no iniciar';
+    if (elemento.classList.contains('iniciar')) {
+        relogio.classList.remove('pausado');
+        clearInterval(timer);
+        iniciaRelogio();
+    }
+    if (elemento.classList.contains('pausar')) {
+        relogio.classList.add('pausado')
+        clearInterval(timer);
+    }
+    if (elemento.classList.contains('zerar')) {
+        relogio.classList.remove('pausado');
+
+        clearInterval(timer);
+        relogio.innerHTML = '00:00:00';
+        segundos = 0;
+    }
 });
-
-pausar.addEventListener('click',function(){
-    alert('click no pausar');
-
-});
-
-zerar.addEventListener('click', function(){
-    alert('click no zerar');
-
-});
-
---
+++ js-tp-basic-to-advanced/Fundamentos/click_event.js
@@ -0,0 +1,5 @@
+const classeBotao = document.querySelector('.classe-do-botao');
+
+classeBotao.addEventListener('click', function(){
+    // executa ao clicar
+})
