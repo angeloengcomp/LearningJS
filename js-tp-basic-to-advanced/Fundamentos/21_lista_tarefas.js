3dc93f7edd2a7c598114bad34ac05bd093d9f28f
Angelo Ricardo <angeloengcomp@gmail.com>
2021-06-10 02:19:54 -0300

    mt sono, bixo

--- js-tp-basic-to-advanced/Fundamentos/21_lista_tarefas.js
+++ js-tp-basic-to-advanced/Fundamentos/21_lista_tarefas.js
@@ -3,15 +3,32 @@ const btnTarefa = document.querySelector('.btn-tarefa');
 const tarefas = document.querySelector('.tarefas');
 
 inputTarefa.addEventListener('keypress', function(e){
-    console.log(e);
-    if(inputTarefa.ke)
+    if(e.keyCode === 13){
+        if(!inputTarefa.value) return;
+        criaTarefa(inputTarefa.value);
+    }
 })
 
+function limpaInput(){
+    inputTarefa.value = '';
+    inputTarefa.focus();
+}
+
+function criaBtnApagar(li){
+    li.innerHTML += ' ';
+    const botaoApagar = document.createElement('button');
+    botaoApagar.innerHTML = 'Apagar';
+    li.appendChild(botaoApagar);
+
+}
 
 function criaTarefa(textoInput) {
     const li = document.createElement('li');//cria elemento li
     li.innerText = textoInput;//elemento li recebe o textinput
     tarefas.appendChild(li);//tarefas recebe um filho li com o textInput
+
+    limpaInput();
+    criaBtnApagar(li)
 }
 
 btnTarefa.addEventListener('click', function (e) {
