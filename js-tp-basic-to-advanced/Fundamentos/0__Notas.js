545b0fc39c655eb592b8628addeadbcd06c270e4
Angelo Ricardo <angeloengcomp@gmail.com>
2021-06-11 01:47:55 -0300

    lista de tarefas ok

---
+++ js-tp-basic-to-advanced/Fundamentos/0__Notas.js
@@ -0,0 +1,4 @@
+// seleciona o botao e seta um atributo nele
+const botaoApagar = document.createElement('button');
+botaoApagar.innerHTML = 'Apagar';
+botaoApagar.setAttribute('class', 'apagar')//seta um atributo
--- js-tp-basic-to-advanced/Fundamentos/21_lista_tarefas.js
+++ js-tp-basic-to-advanced/Fundamentos/21_lista_tarefas.js
@@ -2,36 +2,72 @@ const inputTarefa = document.querySelector('.input-tarefa');
 const btnTarefa = document.querySelector('.btn-tarefa');
 const tarefas = document.querySelector('.tarefas');
 
-inputTarefa.addEventListener('keypress', function(e){
-    if(e.keyCode === 13){
-        if(!inputTarefa.value) return;
+inputTarefa.addEventListener('keypress', function (e) {
+    if (e.keyCode === 13) {
+        if (!inputTarefa.value) return;
         criaTarefa(inputTarefa.value);
     }
 })
 
-function limpaInput(){
+function limpaInput() {
     inputTarefa.value = '';
     inputTarefa.focus();
 }
 
-function criaBtnApagar(li){
+function criaBtnApagar(li) {
     li.innerHTML += ' ';
     const botaoApagar = document.createElement('button');
     botaoApagar.innerHTML = 'Apagar';
+    botaoApagar.setAttribute('class', 'apagar')//seta um atributo
+    botaoApagar.setAttribute('title', 'Apagar esta tarefa')
     li.appendChild(botaoApagar);
+    
+}
 
+function salvarTarefas() {
+    const liTarefas = tarefas.querySelectorAll('li');
+    const listaDeTarefas = [];
+    
+    for (let tarefa of liTarefas){
+        let tarefaTexto = tarefa.innerText;
+        tarefaTexto = tarefaTexto.replace('Apagar', '').trim();
+        listaDeTarefas.push(tarefaTexto);
+    };
+    const tarefasJSON = JSON.stringify(listaDeTarefas);
+    localStorage.setItem('tarefas', tarefasJSON);
 }
 
 function criaTarefa(textoInput) {
     const li = document.createElement('li');//cria elemento li
-    li.innerText = textoInput;//elemento li recebe o textinput
+    li.innerText = textoInput;//elemento li recebe o textinput    
     tarefas.appendChild(li);//tarefas recebe um filho li com o textInput
-
     limpaInput();
     criaBtnApagar(li)
+    salvarTarefas();
 }
 
 btnTarefa.addEventListener('click', function (e) {
     if (!inputTarefa.value) return; //verifica se existe um valor no input para poder executar o resto
     criaTarefa(inputTarefa.value);
 });
+
+document.addEventListener('click', function (e) {
+    const el = e.target;
+    if (el.classList.contains('apagar')) {
+        el.parentElement.remove();//remove o elemento pai de el, ou seja, remove o li inteiro
+    salvarTarefas();
+
+    }
+})
+
+// retorna as teregas salvas no local storage
+function adicionaTarefasSalvas(){
+    const tarefas = localStorage.getItem('tarefas');
+    const listaDeTarefas = JSON.parse(tarefas);
+
+    for(let tarefa of listaDeTarefas){
+        criaTarefa(tarefa);
+    }
+}
+
+adicionaTarefasSalvas();
